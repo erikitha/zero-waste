3b728d41fc804075132518851cc01d50da60b5d4
bruna suzuki <brunasuzuki12@gmail.com>
2022-06-07 06:38:59 -0700

    seed WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,43 @@
 milk = Ingredient.create!(name: "milk", brand: "parmalat", category: "dairy")
 milk2 =Ingredient.create!(name: "milk", brand: "fazenda", category: "dairy")
-chocolate = Ingredient.create!(name: "chocolate", brand: "parmalat", category: "sweet")
+chocolate = Ingredient.create!(name: "chocolate", brand: "nestle", category: "sweet")
+chocolate em pó = Ingredient.create!(name: "chocolate em pó", brand: "nestle", category: "chocolate")
+butter = Ingredient.create!(name: "butter", brand: "president", category: "butter")
+farinha de trigo = Ingredient.create!(name: "butter", brand: "dona benta", category: "butter")
 
-bolochocolate = Recipe.create!(name: "Bolo de Chocolate")
 
-RecipeIngredient.create!(recipe: bolochocolate, ingredient: milk)
-RecipeIngredient.create!(recipe: bolochocolate, ingredient: chocolate)
 
-puts bolochocolate.ingredient
+
+
+
+
+
+
+
+
+
+
+
+chocolatecake = Recipe.create!(name: "Chocolate Cake")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+RecipeIngredient.create!(recipe: chocolatecake, ingredient: milk)
+RecipeIngredient.create!(recipe: chocolatecake, ingredient: chocolate)
