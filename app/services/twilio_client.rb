a44ac44774b8b6e91748cea620c5bb873b2fa55b
juliana lima <akemi.hiruma@gmail.com>
2022-06-09 15:59:42 -0300

    still configuring twilio

---
+++ app/services/twilio_client.rb
@@ -0,0 +1,28 @@
+class TwilioClient
+  attr_reader :client
+
+def initialize
+  @client = Twilio::REST::Client.new account_sid, auth_token
+end
+
+def send_text(user, message)
+  client.api.account.messages.create(
+    from: user.phone,
+    to: phone_number,
+    body: message
+  )
+  # @client.messages.create(
+end
+private
+
+def account_sid
+  Rails.application.credentials.twilio(:account_sid)
+end
+
+def auth_token
+  Rails.application.credentials.twilio(:auth_token)
+end
+
+def phone_number
+  Rails.application.credentials.twilio(:phone_number)
+end
