7bf0b228430a778e7ce0c3afed95825ac4907c72
bruna suzuki <brunasuzuki12@gmail.com>
2022-06-09 11:54:05 -0700

    index ingredients_pantries WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_150856) do
+ActiveRecord::Schema.define(version: 2022_06_09_170554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 2022_06_08_150856) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "url_image"
+    t.integer "spoon_id"
+    t.string "spoon_url"
   end
 
   create_table "users", force: :cascade do |t|
