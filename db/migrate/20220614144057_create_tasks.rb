d1eec1dc73724c93433c5665ab7a75e7d3b3f402
Erica Park <herykr@hotmail.com>
2022-06-14 12:21:01 -0300

    wip controller and model tasks

--- db/migrate/20220614144057_create_tasks.rb
+++ db/migrate/20220614144057_create_tasks.rb
@@ -1,6 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :details
+      t.boolean :completed
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_170554) do
+ActiveRecord::Schema.define(version: 2022_06_14_144057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,15 @@ ActiveRecord::Schema.define(version: 2022_06_09_170554) do
     t.string "url_image"
     t.integer "spoon_id"
     t.string "spoon_url"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
